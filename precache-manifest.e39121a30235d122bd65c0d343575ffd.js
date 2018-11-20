372c5941ae3238291dec6fa6807b5246e8d05c62
Marko <marko.perendic@yahoo.com>
2018-11-20 10:59:13 +0100

    Updates

---
+++ precache-manifest.e39121a30235d122bd65c0d343575ffd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "4b2114068c317f7da02c",
+    "url": "/contactmanager/static/js/main.4b211406.chunk.js"
+  },
+  {
+    "revision": "047a65f81347276f7876",
+    "url": "/contactmanager/static/js/1.047a65f8.chunk.js"
+  },
+  {
+    "revision": "4b2114068c317f7da02c",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "047a65f81347276f7876",
+    "url": "/contactmanager/static/css/1.71fb9e1c.chunk.css"
+  },
+  {
+    "revision": "2a8fa921e08ff0048e3e40c317a867aa",
+    "url": "/contactmanager/index.html"
+  }
+];
